37c77addecc2f79f65b66c9e2e929aa1a0866b95
Bathka Sowmya <bathkasowmya@gmail.com>
2026-06-26 07:50:30 +0530

    Error Solved

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,6 @@
 import '../styles/navbar.css'
 import { useAuth } from '../context/AuthContext'
 
-import { useAuth } from '../context/AuthContext'
-import '../styles/navbar.css'
-
 const Navbar = () => {
   const { user, logout } = useAuth()
 
